58bfbee912088a576a4f10dab97ec89131eee4a1
Frank Dester <frankdester83@gmail.com>
2021-03-30 19:25:03 +0200

    created new branch(develop) to implement java.nio

--- src/com/adventure_Game/LocationMain.java
+++ src/com/adventure_Game/LocationMain.java
@@ -5,48 +5,13 @@ import java.util.Map;
 import java.util.Scanner;
 
 public class LocationMain {
-
+    /*i'll be creating a file to let input data to the locations class*/
+    //private static Map<Integer, Location> locations = new HashMap<Integer, Location>();
+    private static Locations locations = new Locations();
     private static Map <String, String> vocabulary = new HashMap<String,String>();
-    private static Map<Integer, Location> locations = new HashMap<Integer, Location>();
-    private static Map<String, Integer> tempExit = new HashMap<>();
+    //private static Map<String, Integer> tempExit = new HashMap<>();
 
     public static void main(String[] args) {
-        tempExit = new HashMap<>();
-        locations.put(0, new Location(0, "You are sitting in front of a computer learning Java",tempExit));
-
-        tempExit = new HashMap<>();
-        tempExit.put("W", 2);
-        tempExit.put("E", 3);
-        tempExit.put("S", 4);
-        tempExit.put("N", 5);
-        tempExit.put("X", 6);
-        locations.put(1, new Location(1, "You are standing at the end of a road before a small brick building",tempExit));
-
-
-        tempExit = new HashMap<>();
-        tempExit.put("N", 5);
-        locations.put(2, new Location(2, "You are at the top of a hill",tempExit));
-
-
-        tempExit = new HashMap<>();
-        tempExit.put("W", 1);
-        locations.put(3, new Location(3, "You are inside a building, a well house for a small spring",tempExit));
-
-
-        tempExit = new HashMap<>();
-        tempExit.put("N", 1);
-        tempExit.put("W", 2);
-        locations.put(4, new Location(4, "You are in a valley beside a stream",tempExit));
-
-
-        tempExit = new HashMap<>();
-        tempExit.put("S", 1);
-        tempExit.put("W", 2);
-        locations.put(5, new Location(5, "You are in the forest",tempExit));
-
-
-
-
         vocabulary.put("NORTH","N");
         vocabulary.put("SOUTH","S");
         vocabulary.put("WEST","W");
@@ -60,7 +25,9 @@ public class LocationMain {
         int val = 1;
         Scanner scanner = new Scanner(System.in);
         while(true) {
+            /*test to see if class is immutable
             tempExit.remove("W");
+             */
             System.out.println(locations.get(val).getDescription());
             if(val == 0)
                 break;
