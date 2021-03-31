0eb54f0472949c4ceaf48b5c1363bfa89fcf1a6c
Frank Dester <frankdester83@gmail.com>
2021-03-31 21:27:51 +0200

    simplifying try bloc

--- src/com/adventure_Game/Locations.java
+++ src/com/adventure_Game/Locations.java
@@ -8,13 +8,12 @@ import java.util.Map;
 import java.util.Set;
 /* class for input files*/
 public class Locations implements Map<Integer,Location> {
-    private static Map<Integer,Location> locations = new HashMap<Integer,Location>();
+    private static Map<Integer, Location> locations = new HashMap<Integer, Location>();
 
-<<<<<<< HEAD
     public static void main(String[] args) throws IOException {
         //no need of closing locFile using try like this
-        try(FileWriter locFile = new FileWriter("location.txt")){
-            for(Location location : locations.values())
+        try (FileWriter locFile = new FileWriter("location.txt")) {
+            for (Location location : locations.values())
                 locFile.write(location.getLocationID() + ", " + location.getDescription() + "\n");
         }
 
@@ -50,35 +49,10 @@ public class Locations implements Map<Integer,Location> {
 //                e.printStackTrace();
 //            }
 //        }
-
-=======
-    public static void main(String[] args) {
-        FileWriter locFile = null;
-        try{
-            locFile = new FileWriter("location.txt");
-            for(Location location : locations.values())
-                locFile.write(location.getLocationID() + ", " + location.getDescription() + "\n");
-        }catch(IOException e) {
-            System.out.println("In catch block");
-            e.printStackTrace();
-        }finally {
-            System.out.println("In finally block");
-            try {
-                if (locFile != null) {
-                    System.out.println("About to close file...");
-                    locFile.close();
-                }
-            }catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-
->>>>>>> origin/master
     }
-
-    static{//creating an instance
-        Map<String, Integer>tempExit = new HashMap<>();
-        locations.put(0, new Location(0, "You are sitting in front of a computer learning Java",tempExit));
+    static {//creating an instance
+        Map<String, Integer> tempExit = new HashMap<>();
+        locations.put(0, new Location(0, "You are sitting in front of a computer learning Java", tempExit));
 
         tempExit = new HashMap<>();
         tempExit.put("W", 2);
@@ -86,87 +60,89 @@ public class Locations implements Map<Integer,Location> {
         tempExit.put("S", 4);
         tempExit.put("N", 5);
         tempExit.put("X", 6);
-        locations.put(1, new Location(1, "You are standing at the end of a road before a small brick building",tempExit));
+        locations.put(1, new Location(1, "You are standing at the end of a road before a small brick building", tempExit));
 
 
         tempExit = new HashMap<>();
         tempExit.put("N", 5);
-        locations.put(2, new Location(2, "You are at the top of a hill",tempExit));
+        locations.put(2, new Location(2, "You are at the top of a hill", tempExit));
 
 
         tempExit = new HashMap<>();
         tempExit.put("W", 1);
-        locations.put(3, new Location(3, "You are inside a building, a well house for a small spring",tempExit));
+        locations.put(3, new Location(3, "You are inside a building, a well house for a small spring", tempExit));
 
 
         tempExit = new HashMap<>();
         tempExit.put("N", 1);
         tempExit.put("W", 2);
-        locations.put(4, new Location(4, "You are in a valley beside a stream",tempExit));
+        locations.put(4, new Location(4, "You are in a valley beside a stream", tempExit));
 
 
         tempExit = new HashMap<>();
         tempExit.put("S", 1);
         tempExit.put("W", 2);
-        locations.put(5, new Location(5, "You are in the forest",tempExit));
+        locations.put(5, new Location(5, "You are in the forest", tempExit));
     }
     @Override
-    public int size() {
+    public int size () {
         return locations.size();
     }
 
     @Override
-    public boolean isEmpty() {
+    public boolean isEmpty () {
         return locations.isEmpty();
     }
 
     @Override
-    public boolean containsKey(Object key) {
+    public boolean containsKey (Object key){
         return locations.containsKey(key);
     }
 
     @Override
-    public boolean containsValue(Object value) {
+    public boolean containsValue (Object value){
         return locations.containsValue(value);
     }
 
     @Override
-    public Location get(Object key) {
+    public Location get (Object key){
         return locations.get(key);
     }
 
     @Override
-    public Location put(Integer key, Location value) {
-        return locations.put(key,value);
+    public Location put (Integer key, Location value){
+        return locations.put(key, value);
     }
 
     @Override
-    public Location remove(Object key) {
+    public Location remove (Object key){
         return locations.remove(key);
     }
 
     @Override
-    public void putAll(Map<? extends Integer, ? extends Location> m) {
+    public void putAll (Map < ? extends Integer, ? extends Location > m){
 
     }
 
     @Override
-    public void clear() {
+    public void clear () {
         locations.clear();
     }
 
     @Override
-    public Set<Integer> keySet() {
+    public Set<Integer> keySet () {
         return locations.keySet();
     }
 
     @Override
-    public Collection<Location> values() {
+    public Collection<Location> values () {
         return locations.values();
     }
-
     @Override
-    public Set<Entry<Integer, Location>> entrySet() {
+    public Set<Entry<Integer, Location>> entrySet () {
         return locations.entrySet();
     }
+
 }
+
+
